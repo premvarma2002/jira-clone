347992af52f2a189e202638aef4d91a94b22ef0f
premvarma2002 <vakilverma1982@gmail.com>
2024-10-15 21:12:19 +0530

    fixed component errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
    <div className="flex gap-4">
+    <Input/>
      <Button size="xs">Secondary</Button>
      <Button variant="destructive">Secondary</Button>
      <Button variant="secondary">Secondary</Button>
