41c0b9a32d1432d1f12847aaf342fbc7388dc2f2
premvarma2002 <vakilverma1982@gmail.com>
2024-11-19 13:59:29 +0530

    Added Sidebar and Mobile Sidebar

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,5 +1,3 @@
-
-import { UserButton } from "@/features/auth/components/user-button";
 import { getCurrent } from "@/features/auth/server/actions";
 import { redirect } from "next/navigation";
 export default async function Home() {
@@ -8,7 +6,7 @@ export default async function Home() {
   if (!user) redirect("/sign-in");
   return (
    <div>
-   <UserButton/>
+      This is a homepage
    </div>
   );
 };
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import { UserButton } from "@/features/auth/components/user-button";
+import { MobileSidebar } from "./mobile-sidebar";
 
 export const Navbar = () => {
   return (
-    <nav>Navbar</nav>
-  )
-}
+    <nav className='pt-4 px-6 flex items-center justify-between'>
+      <div className='flex-col hidden lg:flex'>
+       <h1 className="text-2xl font-semibold">Home</h1>
+       <p className="text-muted-foregound">Monitor all of your projects and tasks here</p>
+      </div>
+      <MobileSidebar/>
+      <UserButton/>
+      </nav>
+  );
+};
