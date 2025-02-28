27a49b7e287932b9a008b7a36c243d7adabb257c
premvarma2002 <vakilverma1982@gmail.com>
2025-02-28 17:46:39 +0530

    Create Workspace

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google"
 import { cn } from "@/lib/utils";
 import { QueryProvider } from "@/components/query-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 import "./globals.css";
 
@@ -23,6 +24,7 @@ export default function RootLayout({
        className={cn(inter.className, "antialiased min-h-screen")}
       >
         <QueryProvider>
+          <Toaster/>
         {children}
         </QueryProvider>
       </body>
--- src/components/mobile-sidebar.tsx
+++ src/components/mobile-sidebar.tsx
@@ -1,14 +1,22 @@
 'use client';
 
+import { useEffect, useState } from "react";
 import { MenuIcon } from "lucide-react";
 import { Button } from "./ui/button";
 
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import { Sidebar } from "./sidebar";
+import { usePathname } from "next/navigation";
 
 export const MobileSidebar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    setIsOpen(false);
+  }, [pathname]);
     return (
-      <Sheet modal={false}>
+      <Sheet modal={false} open={isOpen} onOpenChange={setIsOpen}>
        <SheetTrigger asChild>
         <Button variant="secondary" className="lg:hidden">
             <MenuIcon className="size-5 text-neutral-500"/>
